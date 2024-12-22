6b1d598809bb1e9da23a2ff8b7b8cd9b9eb4e20c
Ronal <ronal.sanx21@gmail.com>
2024-12-22 12:10:45 -0500

    Copy

--- src/ui/Layout.jsx
+++ src/ui/Layout.jsx
@@ -12,6 +12,10 @@ export const Layout = () => {
       <Acerca/>
       <hr/>
       <Tecnologias/>
+
+      <div className="text-white text-center text-gray-400">
+        <p>&copy; 2024 Diseñado y programado por  <a href="https://linktr.ee/sronaalz" target="_blank">Ronal Sánchez</a></p>
+      </div>
     </>
   )
 }
